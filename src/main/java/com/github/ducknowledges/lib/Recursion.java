14096bde45f6945892634c9fc751284c6a0a7fc9
ducknowledges <ducknowledges@gmail.com>
2024-01-28 19:51:52 +0500

    feat[recurseive-pow]: add recursion raising to the power

---
+++ src/main/java/com/github/ducknowledges/lib/Recursion.java
@@ -0,0 +1,13 @@
+package com.github.ducknowledges.lib;
+
+public class Recursion {
+
+  private Recursion() {}
+
+  public static int recursionPow(int base, int power) {
+    if (power == 0) {
+      return 1;
+    }
+    return base * recursionPow(base, power - 1);
+  }
+}
---
+++ src/test/java/com/github/ducknowledges/lib/RecursionTest.java
@@ -0,0 +1,19 @@
+package com.github.ducknowledges.lib;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RecursionTest {
+
+  @ParameterizedTest
+  @CsvSource({"2, 0, 1", "2, 1, 2", "2, 2, 4", "2, 3, 8", "2, 4, 16"})
+  @DisplayName("should return the correct number for recursion power calculation")
+  void shouldCorrectlyRecusiveRaisedToThePower(int base, int power, int expected) {
+    int actual = Recursion.recursionPow(base, power);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+}
